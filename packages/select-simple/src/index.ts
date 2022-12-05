a2ab0b2d34f852bd97b34069b8aeb6ecd5af895d
VovanSuper <ovsyukov@yandex.com>
2022-12-05 12:14:41 +0300

    fix: return Name to lib package.json

--- packages/select-simple/src/index.ts
+++ packages/select-simple/src/index.ts
@@ -1,4 +1,4 @@
 export { SelectItemComponent } from './lib/select-item.component';
-export * from './lib/select-simple.module';
+export { SelectSimpleModule } from './lib/select-simple.module';
 export { SelectComponent } from './lib/select.component';
 export { BasicStylesSet, IOption, IOptionClickEvent, ISelectItem, ITemplates } from './lib/settings/models';
