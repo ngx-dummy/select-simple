701d96e75bb3cab48c0c04602ec847d01de621b3
VovanSuper <ovsyukov@yandex.com>
2022-12-08 17:53:52 +0300

    Release %s

---
+++ scripts/docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Preparing a docs ...
+
+ROOT=$(pwd)
+DOCS_DIR=$ROOT/docs
+
+if [[ ! -e $DOCS_DIR ]]; then
+    mkdir $DOCS_DIR
+elif [[ ! -d $DOCS_DIR ]]; then
+    echo "$DOCS_DIR already exists but is not a directory" 1>&2
+fi
+
+cp $ROOT/.nojekyll $DOCS_DIR
+npm run build:docs
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+echo Preparing a new Release ...
+
 ROOT=$(pwd)
-WORKING_DIR=$ROOT/dist/packages/select-simple
+LIB_DIR=$ROOT/packages/select-simple
 
-if [[ ! -e $WORKING_DIR ]]; then
-    mkdir -p $WORKING_DIR
-fi
+pushd $LIB_DIR/
+echo Patching Select-simple Package.json version ...
+npm version patch --git-tag-version=false
+popd
 
-echo "Building the library..."
-npm run build:prod
-
-echo "Copying readme, license to the library destination..."
-cp -ruf $ROOT/README.md $WORKING_DIR/
-cp -ruf $ROOT/LICENSE $WORKING_DIR/
+echo Patching root Package.json version and committing Release version...
+npm version patch --git-tag-version=false --message 'Release %s'
+git add .
+git commit -am 'Release %s'
+git push origin --no-verify
