550d8a18d0241134e1e47fdf77b51a10d0305fc6
VovanSuper <ovsyukov@yandex.com>
2022-12-09 11:07:47 +0300

    Release v0.0.16

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,17 @@ echo Preparing a new Release ...
 ROOT=$(pwd)
 LIB_DIR=$ROOT/packages/select-simple
 
-pushd $LIB_DIR/
-echo Patching Select-simple Package.json version ...
-npm version patch --git-tag-version=false
-popd
+# pushd $LIB_DIR/
+# echo Patching Select-simple Package.json version ...
+# npm version patch --git-tag-version=false
+# popd
 
 echo Patching root Package.json version and committing Release version...
-git add .
-npm version patch --git-tag-version=false --message "Release %s" | git commit -am "awk '{print $1}'"
-git push origin --no-verify
+
+RELEASE_VERSION=$(npm version patch --git-tag-version=false --message 'Release %s')
+
+# git add .
+
+echo Release $RELEASE_VERSION 
+git commit -am "Release $RELEASE_VERSION"
+# git push origin --no-verify
