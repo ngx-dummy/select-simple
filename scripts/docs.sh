4576190541d8956bdb4486f26491f95639c3818f
VovanSuper <ovsyukov@yandex.com>
2022-12-09 11:10:05 +0300

    Release

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,7 +3,7 @@
 echo Preparing a docs ...
 
 ROOT=$(pwd)
-DOCS_DIR=$ROOT/docs
+DOCS_DIR=/docs
 
 if [[ ! -e $DOCS_DIR ]]; then
     mkdir $DOCS_DIR
--- scripts/release.sh
+++ scripts/release.sh
@@ -5,17 +5,17 @@ echo Preparing a new Release ...
 ROOT=$(pwd)
 LIB_DIR=$ROOT/packages/select-simple
 
-# pushd $LIB_DIR/
-# echo Patching Select-simple Package.json version ...
-# npm version patch --git-tag-version=false
-# popd
+pushd $LIB_DIR/
+echo Patching Select-simple Package.json version ...
+npm version patch --git-tag-version=false
+popd
 
 echo Patching root Package.json version and committing Release version...
 
 RELEASE_VERSION=$(npm version patch --git-tag-version=false --message 'Release %s')
 
-# git add .
+git add .
 
 echo Release $RELEASE_VERSION 
 git commit -am "Release $RELEASE_VERSION"
-# git push origin --no-verify
+git push origin --no-verify
